def6fb0ee168ee8f670c48fa7aa4f4956292ea8b
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-10 19:22:35 -0800

    move gather to CPU

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -517,6 +517,7 @@ class GroupCoordinator:
         if dim < 0:
             # Convert negative dim to positive.
             dim += input_.dim()
+        input_.cpu()
         # Allocate output tensor.
         if self.rank_in_group == dst:
             gather_list = [torch.empty_like(input_) for _ in range(world_size)]
@@ -524,7 +525,7 @@ class GroupCoordinator:
             gather_list = None
         # Gather.
         print("gather is issued")
-        torch.distributed.gather(input_.cpu(),
+        torch.distributed.gather(input_,
                                  gather_list,
                                  dst=self.ranks[dst],
                                  group=self.cpu_group)
