69a1713bca72db5e40d20a4a30f46b28347dcd77
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-19 15:31:28 -0800

    implement all-to-all

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -261,7 +261,7 @@ class LlamaAttention(nn.Module):
         # dist.all_to_all_single(recvbuf_TP, sendbuf_TP, group=group_TP)
         # dist.all_reduce(q, group=group_TP)
         # dist.all_reduce(q, group_SP)
-        get_tp_group().all_reduce(q, get_tp_group().device_group)
+        get_tp_group().all_reduce(q)
 
         # torch.cuda.synchronize()
         # dist.barrier()
