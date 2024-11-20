244b581b7130c720ce27dd1de6383564e6af90ce
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-19 23:16:04 -0800

    implement irregular all-to-all(SP)

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -216,29 +216,19 @@ class LlamaAttention(nn.Module):
             print(f"original q {q.shape}, k {k.shape}, v {v.shape}")
 
         # send buffers
-        q = torch.transpose(q.reshape((N_ulysses, SP, d//SP//TP)), 0, 1)
-        k = torch.transpose(k.reshape((N_ulysses, SP, d_kv//SP//TP)), 0, 1)
-        v = torch.transpose(v.reshape((N_ulysses, SP, d_kv//SP//TP)), 0, 1)
-        qkv = torch.cat([q, k, v], dim=-1)
+        q = q.reshape((N_ulysses, SP, d//SP//TP))
+        k = k.reshape((N_ulysses, SP, d_kv//SP//TP))
+        v = v.reshape((N_ulysses, SP, d_kv//SP//TP))
+        qkv = torch.cat([q, k, v], dim=-1).transpose(0, 1).contiguous()
         qkv_ = torch.empty((N, (d + 2*d_kv)//SP//TP), dtype=hidden_states.dtype, device=hidden_states.device)
+
+        dist.all_to_all_single(qkv_, qkv, output_split_sizes=N_ranks, group=get_sp_group().device_group)
+        q_, k_, v_ = qkv_.split([d//SP//TP, d_kv//SP//TP, d_kv//SP//TP], dim=-1)
 
         if dist.get_rank() == 0:
             print(f"llama attention q {q.shape}, k {k.shape}, v {v.shape}")
             print(f"llama attention qkv {qkv.shape} is_contiguous {qkv.is_contiguous()}")
             print(f"llama attention qkv_ {qkv_.shape} is_contiguous {qkv_.is_contiguous()}")
-
-        dist.all_to_all_single(qkv_, qkv, output_split_sizes=N_ranks, group=get_sp_group().device_group)
-        # dist.all_to_all_single(q_, q, output_split_sizes=N_ranks, group=get_sp_group().device_group)
-        # dist.all_to_all_single(k_, k, output_split_sizes=N_ranks, group=get_sp_group().device_group)
-        # dist.all_to_all_single(v_, v, output_split_sizes=N_ranks, group=get_sp_group().device_group)
-
-        # receive buffers
-        q_, k_, v_ = qkv_.split([d//SP//TP, d_kv//SP//TP, d_kv//SP//TP], dim=-1)
-        # q_ = torch.empty((N, d//SP//TP), dtype=hidden_states.dtype, device=hidden_states.device)
-        # k_ = torch.empty((N, d_kv//SP//TP), dtype=hidden_states.dtype, device=hidden_states.device)
-        # v_ = torch.empty((N, d_kv//SP//TP), dtype=hidden_states.dtype, device=hidden_states.device)
-        
-        if dist.get_rank() == 0:
             print(f"llama attention q_ {q_.shape}, k_ {k_.shape}, v_ {v_.shape}")
         
         attn_output = self.attn(q_, k_, v_, kv_cache, attn_metadata)
