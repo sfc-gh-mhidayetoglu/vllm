7a494edcf5ac08ac493875207a5bf9ad8bbcf1d1
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-19 22:43:14 -0800

    implement irregular all-to-all(SP)

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -223,6 +223,10 @@ class LlamaAttention(nn.Module):
         k_ = torch.empty((N, d_kv//SP//TP), dtype=hidden_states.dtype, device=hidden_states.device)
         v_ = torch.empty((N, d_kv//SP//TP), dtype=hidden_states.dtype, device=hidden_states.device)
 
+        if dist.get_rank() == 0:
+            print(f"llama attention q {q.shape}, k {k.shape}, v {v.shape}")
+            print(f"llama attention q_ {q_.shape}, k_ {k_.shape}, v_ {v_.shape
+
         dist.all_to_all_single(q_, q, output_split_sizes=N_ranks, group=get_sp_group().device_group)
         dist.all_to_all_single(k_, k, output_split_sizes=N_ranks, group=get_sp_group().device_group)
         dist.all_to_all_single(v_, v, output_split_sizes=N_ranks, group=get_sp_group().device_group)
