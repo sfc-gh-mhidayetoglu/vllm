beef0a4811b8af0d810a1fa83c27671274182f40
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-20 22:53:39 -0800

    debug

--- vllm/executor/multiproc_gpu_executor.py
+++ vllm/executor/multiproc_gpu_executor.py
@@ -179,11 +179,11 @@ class MultiprocessingGPUExecutor(DistributedGPUExecutor):
             raise NotImplementedError(
                 "max_concurrent_workers is not supported yet.")
 
-        if torch.distributed.is_initialized():
-            torch.cuda.synchronize()
-            torch.distributed.barrier()
-            if torch.distributed.get_rank() == 0:
-                print(f"test **************************************************************************************** async_run_tensor_parallel_workers_only {async_run_tensor_parallel_workers_only}")
+        # if torch.distributed.is_initialized():
+        #     torch.cuda.synchronize()
+        #     torch.distributed.barrier()
+        #     if torch.distributed.get_rank() == 0:
+        print(f"test **************************************************************************************** async_run_tensor_parallel_workers_only {async_run_tensor_parallel_workers_only}")
 
         if async_run_tensor_parallel_workers_only:
             # Run only non-driver workers and just return futures.
@@ -192,11 +192,11 @@ class MultiprocessingGPUExecutor(DistributedGPUExecutor):
                 for worker in self.non_driver_workers
             ]
         
-        if torch.distributed.is_initialized():
-            torch.cuda.synchronize()
-            torch.distributed.barrier()
-            if torch.distributed.get_rank() == 0:
-                print("test 2 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+        # if torch.distributed.is_initialized():
+        #     torch.cuda.synchronize()
+        #     torch.distributed.barrier()
+        #     if torch.distributed.get_rank() == 0:
+        print("test 2 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
 
 
         # Start all remote workers first.
