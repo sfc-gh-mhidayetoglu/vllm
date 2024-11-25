bf0a98e60cb51694f8726a806d72d84241efb327
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-24 23:08:03 -0800

    track down the hang

--- vllm/worker/worker_base.py
+++ vllm/worker/worker_base.py
@@ -332,6 +332,7 @@ class LocalOrDistributedWorkerBase(WorkerBase):
             print(f"after prepare_input {type(inputs)}", flush=True)
         torch.cuda.synchronize()
         torch.distributed.barrier()
+        exit()
 
         model_input, worker_input, kwargs = inputs
         num_steps = worker_input.num_steps
