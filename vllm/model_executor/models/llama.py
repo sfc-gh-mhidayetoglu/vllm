11c0df8cf9e6c31752144cc99575ec9b290c1a3f
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-24 19:49:18 -0800

    implement ulysses attention

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -113,7 +113,8 @@ class LlamaAttention(nn.Module):
     ) -> None:
         super().__init__()
         self.hidden_size = hidden_size
-        tp_size = get_tensor_model_parallel_world_size()
+        # tp_size = get_tensor_model_parallel_world_size()
+        tp_size = get_tp_ulysses_group().world_size
         self.total_num_heads = num_heads
         assert self.total_num_heads % tp_size == 0
         self.num_heads = self.total_num_heads // tp_size
