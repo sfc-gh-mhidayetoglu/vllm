996a695d6f2cda064ee71c9af560bac34ecee08f
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-25 21:17:52 -0800

    try two responses

--- vllm/worker/worker_base.py
+++ vllm/worker/worker_base.py
@@ -405,7 +405,6 @@ class LocalOrDistributedWorkerBase(WorkerBase):
         torch.distributed.barrier()
         if torch.distributed.get_rank() == 0:
             print("model is executed")
-        exit()
 
         # output is List[SamplerOutput]
         return output
