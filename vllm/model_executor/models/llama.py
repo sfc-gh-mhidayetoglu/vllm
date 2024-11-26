0091c0750312a92d15a1c2a216c0317f6265cb0f
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-11-26 01:36:15 -0800

    print hidden_states shape

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -473,9 +473,9 @@ class LlamaModel(nn.Module):
 
 
         # all-gather sequences
-        # hidden_states_list = [torch.empty((N_ranks[i], hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device) for i in range(SP)]
-        # torch.distributed.all_gather(hidden_states_list, hidden_states, group=get_sp_group().device_group)
-        # hidden_states = torch.cat(hidden_states_list)
+        hidden_states_list = [torch.empty((N_ranks[i], hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device) for i in range(SP)]
+        torch.distributed.all_gather(hidden_states_list, hidden_states, group=get_sp_group().device_group)
+        hidden_states = torch.cat(hidden_states_list)
 
         torch.cuda.synchronize()
         torch.distributed.barrier()
