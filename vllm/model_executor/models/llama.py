22ebf9b9f3cf18cddbfe55000dbef06eadf284d7
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-02 19:57:42 -0800

    exit after all-gather

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -482,7 +482,7 @@ class LlamaModel(nn.Module):
         torch.cuda.synchronize()
         torch.distributed.barrier()
         print(f"myid {torch.distributed.get_rank()} numforward {self.numforward}\n", flush=True)
-        if self.numforward == 3:
+        if self.numforward == 2:
             exit()
         self.numforward += 1
 
