94a7a4f47951543c960c0100c1bc35601dabc146
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-03 00:42:10 -0800

    exit after prune

--- vllm/model_executor/layers/logits_processor.py
+++ vllm/model_executor/layers/logits_processor.py
@@ -57,7 +57,7 @@ class LogitsProcessor(nn.Module):
         torch.cuda.synchronize()
         torch.distributed.barrier()
         # if torch.distributed.get_rank() == 0:
-        print(f"myid {torch.distributed.get_rank()} LogitsProcessor {self.numforward} hidden_states shape {hidden_states.shape} logits_as_input {self.logits_as_input}\n", flush=True)
+        # print(f"myid {torch.distributed.get_rank()} LogitsProcessor {self.numforward} hidden_states shape {hidden_states.shape} logits_as_input {self.logits_as_input}\n", flush=True)
 
 
         if self.logits_as_input:
@@ -65,8 +65,8 @@ class LogitsProcessor(nn.Module):
         else:
             torch.cuda.synchronize()
             torch.distributed.barrier()
-            if torch.distributed.get_rank() == 0:
-                print(f"hidden_states before pruning shape {hidden_states.shape} sampling_metadata type {type(sampling_metadata)} {sampling_metadata.selected_token_indices}", flush=True)
+            # if torch.distributed.get_rank() == 0:
+            print(f"hidden_states before pruning shape {hidden_states.shape} sampling_metadata type {type(sampling_metadata)} {sampling_metadata.selected_token_indices}", flush=True)
             if self.numforward == 2:
                 exit()
             hidden_states = _prune_hidden_states(hidden_states,
