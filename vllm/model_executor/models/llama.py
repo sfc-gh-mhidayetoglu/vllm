6bffe2bdc9cd0bdab9c65ad4144150ac27b99604
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-02 22:32:34 -0800

    create empty hidden_states

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -477,7 +477,7 @@ class LlamaModel(nn.Module):
         # hidden_states_ = torch.split(torch.empty((sum(N_ranks), hidden_states.shape[1]), device=hidden_states.device, dtype=hidden_states.dtype), N_ranks)
         hidden_states_list = [torch.empty((N_ranks[i], hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device) for i in range(SP)]
         print(f"myid {torch.distributed.get_rank()} sp_group ranks {get_sp_group().ranks}", flush=True)
-        # torch.distributed.all_gather(hidden_states_list, hidden_states, group=get_sp_group().device_group)
+        torch.distributed.all_gather(hidden_states_list, hidden_states, group=get_sp_group().device_group)
         # print(f"myid {torch.distributed.get_rank()} {[hidden_states_list[i].shape in range(SP)]}\n", flush=True)
         # hidden_states = torch.empty((sum(N_ranks), hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device)
 
