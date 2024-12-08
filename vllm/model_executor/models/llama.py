10d78bb8a8f63d1064fc1aef025d8ad02f7d0edc
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-08 00:57:22 -0800

    exclude 0-size tensor from computation

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -277,10 +277,10 @@ class LlamaAttention(nn.Module):
         c = torch.transpose(c, 0, 1).reshape(N_ulysses, self.q_size)
 
         # output projection
-        if hidden_states.shape[0] > 0:
-            output, _ = self.o_proj(c)
-        else:
-            output = torch.empty((0, self.hidden_size), dtype=hidden_states.dtype, device=hidden_states.device)
+        # if hidden_states.shape[0] > 0:
+        output, _ = self.o_proj(c)
+        # else:
+        #     output = torch.empty((0, self.hidden_size), dtype=hidden_states.dtype, device=hidden_states.device)
 
 
         torch.cuda.synchronize()
