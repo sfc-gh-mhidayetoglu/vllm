14183098cdd7f1c6d5d3786a06bbd25a35c47bfe
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-16 22:13:15 -0800

    clean async_llm_engine.py

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -647,7 +647,6 @@ class AsyncLLMEngine(EngineClient):
         else:
             from vllm.executor.gpu_executor import GPUExecutorAsync
             executor_class = GPUExecutorAsync
-        logger.info("Using Async LLMEngine executor class: %s", executor_class)
         return executor_class
 
     @classmethod
