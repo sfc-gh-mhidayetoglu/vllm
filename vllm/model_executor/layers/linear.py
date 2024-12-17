ca09f7c4048c5bcd75a75ac75b216c78db793341
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-17 14:38:05 -0800

    clean linear.py

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -1078,7 +1078,7 @@ class RowParallelLinear(LinearBase):
         # bias will not get added more than once in TP>1 case)
         bias_ = None if (self.tp_rank > 0 or self.skip_bias_add) else self.bias
         output_parallel = self.quant_method.apply(self,
-                                                  input_parallel, 
+                                                  input_parallel,
                                                   bias=bias_)
         if self.reduce_results and self.tp_size > 1:
             output = tensor_model_parallel_all_reduce(output_parallel)
