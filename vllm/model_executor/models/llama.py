4073659532f4f27ec0fd5b7ccb7ac328f20cf6e4
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-23 22:04:23 -0800

    print number of model forwards

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -225,8 +225,8 @@ class LlamaAttention(nn.Module):
         c = torch.empty((SP, N_ulysses, self.q_size//SP), dtype=hidden_states.dtype, device=hidden_states.device)
         if torch.distributed.get_rank() == 0:
             print(f"*** run attention {self.numattention}")
-        # if self.numattention != 171:
-        torch.distributed.all_to_all_single(c, attn_output, input_split_sizes=N_ranks, group=get_sp_group().device_group)
+        if self.numattention != 171:
+            torch.distributed.all_to_all_single(c, attn_output, input_split_sizes=N_ranks, group=get_sp_group().device_group)
         c = torch.transpose(c, 0, 1).reshape(N_ulysses, self.q_size)
 
         # output projection
