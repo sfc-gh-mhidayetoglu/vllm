dd8db9171334e4bee2397903a60f4d1642447c38
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-23 17:44:16 -0800

    find the hang in cuda graph

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -398,8 +398,13 @@ class LlamaModel(nn.Module):
         # narrow hidden_states
         hidden_states = torch.narrow(hidden_states, 0, sum(N_ranks[:SP_rank]), N_ranks[SP_rank]).clone()
 
-        if torch.distributed.get_rank() == 0:
-            print(f"*** run model 0 hidden_states shape: {hidden_states.shape} residual shape: {residual.shape if residual is not None else None}")
+        torch.cuda.synchronize()
+        torch.distruibted.barrier()
+        for i in range(torch.distributed.get_world_size())
+            if i == torch.distributed.get_rank():
+                print(f"*** run model hidden_states shape: {hidden_states.shape} residual shape: {residual.shape if residual is not None else None}")
+            torch.cuda.synchronize()
+            torch.distributed.barrier()
         for i in range(self.start_layer, self.end_layer):
             layer = self.layers[i]
             # if torch.distributed.get_rank() == 0:
