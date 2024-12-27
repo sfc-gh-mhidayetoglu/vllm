c4d8148996df8d8dd6c878620f50f3396ba62c11
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-26 18:36:31 -0600

    narrow hidden_states into hidden_states_ulysses

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -425,7 +425,7 @@ class LlamaModel(nn.Module):
 
         # narrow hidden_states
         hidden_states_ulysses = torch.empty((N_ulysses, hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device)
-        hidden_states_ulysses[:N_ulysses] = hidden_states.narrow(0, sum(N_ranks[:SP_rank]), N_ranks[SP_rank])
+        hidden_states_ulysses[:N_ranks(SP_rank)] = hidden_states.narrow(0, sum(N_ranks[:SP_rank]), N_ranks[SP_rank])
 
         # hidden_states_list = [torch.empty((N_ranks[i], hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device) for i in range(SP)]
         hidden_states = torch.empty((SP*N_ulysses, hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device)
