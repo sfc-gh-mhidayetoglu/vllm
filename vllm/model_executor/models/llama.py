06f26f0c97cbdfabedea8fb6fa4db159b5f584ea
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-27 00:33:36 -0600

    comment out all-gather

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -406,8 +406,9 @@ class LlamaModel(nn.Module):
         #     print(f"*** run model seq_lengths: {N_ranks} total length {N}")
 
         # narrow hidden_states
-        hidden_states = torch.empty((N_ulysses, hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device)
+        # hidden_states = torch.empty((N_ulysses, hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device)
         # hidden_states_ulysses[:N_ranks[SP_rank]] = hidden_states.narrow(0, sum(N_ranks[:SP_rank]), N_ranks[SP_rank])
+        hidden_states = torch.narrow(hidden_states, 0, 0, N_ulysses)
 
         # all-gather hidden_states
         hidden_states_list = torch.empty((SP * N_ulysses, hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device)
