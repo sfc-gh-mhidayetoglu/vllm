63d5c5355927bea2cbf6b9229598f6efe1a4084c
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-26 23:59:55 -0600

    comment out all-gather

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -411,7 +411,7 @@ class LlamaModel(nn.Module):
 
         # all-gather hidden_states
         hidden_states_list = torch.empty((SP * N_ulysses, hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device)
-        torch.distributed.all_gather_into_tensor(hidden_states_list, hidden_states, group=get_sp_group().device_group)
+        # torch.distributed.all_gather_into_tensor(hidden_states_list, hidden_states, group=get_sp_group().device_group)
         hidden_states = torch.narrow(hidden_states_list, 0, 0, N)
 
         return hidden_states
