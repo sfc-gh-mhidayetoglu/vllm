c006be79a29f46a06e0643e7de407a9a3ae81a5e
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-30 17:33:36 -0600

    remove clone

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -396,7 +396,7 @@ class LlamaModel(nn.Module):
         SP_rank = get_sp_group().rank_in_group
 
         # narrow hidden_states
-        hidden_states = torch.narrow(hidden_states, 0, sum(N_ranks[:SP_rank]), N_ranks[SP_rank]).clone()
+        hidden_states = torch.narrow(hidden_states, 0, sum(N_ranks[:SP_rank]), N_ranks[SP_rank])#.clone()
         if torch.distributed.get_rank() == 0:
             print(f"model forward N_ranks: {N_ranks} N: {N}")
 
