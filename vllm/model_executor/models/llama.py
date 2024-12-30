daa57c1656c3ff5a8b61e4de108768158fe288f3
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-30 16:07:17 -0600

    print N

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -417,7 +417,7 @@ class LlamaModel(nn.Module):
 
         # all-gather hidden_states
         hidden_states_list = [torch.empty((N_ranks[i], hidden_states.shape[1]), dtype=hidden_states.dtype, device=hidden_states.device) for i in range(SP)]
-        torch.distributed.all_gather(hidden_states_list, hidden_states, group=get_sp_group().device_group)
+        # torch.distributed.all_gather(hidden_states_list, hidden_states, group=get_sp_group().device_group)
         hidden_states = torch.cat(hidden_states_list)
 
         return hidden_states
