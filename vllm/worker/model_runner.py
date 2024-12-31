81bc674caa217969b2f2c52c811a2a3ce657554a
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-31 13:31:04 -0600

    set self.model

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -1664,7 +1664,7 @@ class ModelRunner(GPUModelRunnerBase[ModelInputForGPUWithSamplingMetadata]):
                 torch.cuda.synchronize()
                 torch.distributed.barrier()
 
-        # model_executable = self.model
+        model_executable = self.model
 
         # torch.cuda.synchronize()
         # torch.distributed.barrier()
