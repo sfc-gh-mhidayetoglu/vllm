81e5355b32ff1e05ec854455a96d91aaed62bb8a
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2024-12-31 14:57:26 -0600

    use_custom_allreduce=False

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -1077,6 +1077,7 @@ def initialize_model_parallel(
                                         get_world_group().local_rank,
                                         backend,
                                         use_custom_allreduce=False,
+                                        use_message_queue_broadcaster=True,
                                         group_name="sp_tp")
 
 def ensure_model_parallel_initialized(
