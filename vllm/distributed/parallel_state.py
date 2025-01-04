979d8f5cff8407c0952040ed29ada2b8465e9998
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-01-03 23:23:36 -0800

    comment out distributed.all_reduce

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -361,7 +361,7 @@ class GroupCoordinator:
 
     def _all_reduce_out_place(self, input_: torch.Tensor) -> torch.Tensor:
         if torch.distributed.get_rank() == 0:
-            print(f"ca_comm.custom_all_reduce {input.shape}")
+            print(f"ca_comm.custom_all_reduce {input_.shape}")
         ca_comm = self.ca_comm
         assert ca_comm is not None
         assert not ca_comm.disabled
@@ -373,14 +373,14 @@ class GroupCoordinator:
         pynccl_comm = self.pynccl_comm
         if (pynccl_comm is not None and not pynccl_comm.disabled):
             if torch.distributed.get_rank() == 0:
-                print(f"pynccl_comm.all_reduce {input.shape}")
+                print(f"pynccl_comm.all_reduce {input_.shape}")
             pynccl_comm.all_reduce(input_)
         elif input_.is_cpu:
             import intel_extension_for_pytorch as ipex
             ipex.distributed.all_reduce(input_, group=self.device_group)
         else:
             if torch.distributed.get_rank() == 0:
-                print(f"torch.distributed.all_reduce {input.shape}")
+                print(f"torch.distributed.all_reduce {input_.shape}")
             torch.distributed.all_reduce(input_, group=self.device_group)
 
     def all_gather(self, input_: torch.Tensor, dim: int = -1) -> torch.Tensor:
