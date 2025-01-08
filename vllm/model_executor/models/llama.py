6dfd51fbd8c76f25e76bac01d065cf45121fae4c
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-01-08 13:42:04 -0800

    return hidden_states

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -388,6 +388,8 @@ class LlamaModel(nn.Module):
             hidden_states = intermediate_tensors["hidden_states"]
             residual = intermediate_tensors["residual"]
 
+        return hidden_states
+
         N = len(input_ids)
         SP = get_sp_group().world_size
         N_ranks = [N//SP]*SP
