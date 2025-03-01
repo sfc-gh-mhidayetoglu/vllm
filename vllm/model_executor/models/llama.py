d3f628fc523eae70097b4ae1e5cd807701d3e8cf
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-01 15:17:46 -0800

    print numforward

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -539,6 +539,8 @@ class LlamaForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
         self.make_empty_intermediate_tensors = (
             self.model.make_empty_intermediate_tensors)
 
+        self.numforward = 0
+
     def _init_model(self, vllm_config: VllmConfig, prefix: str = ""):
         return LlamaModel(vllm_config=vllm_config, prefix=prefix)
 
@@ -570,12 +572,12 @@ class LlamaForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
         #     print(f"input_ids: {input_ids.shape}")
         #     print(f"positions: {positions.shape}")
         #     print(f"N {N}, SP {SP}, N_ranks {N_ranks} sum {sum(N_ranks)}")
-        # from vllm.forward_context import get_forward_context
-        # if torch.distributed.get_rank() == 0:
-        #     print(f"numforward {self.numforward} N {N} "
-        #           f"N_ranks {N_ranks}")
-        # if get_forward_context().attn_metadata is not None:
-        #     self.numforward += 1
+        from vllm.forward_context import get_forward_context
+        if torch.distributed.get_rank() == 0:
+            print(f"numforward {self.numforward} N {N} "
+                  f"N_ranks {N_ranks}")
+        if get_forward_context().attn_metadata is not None:
+            self.numforward += 1
 
         input_ids[0:N_ulysses] = input_ids[N_start:N_start + N_ulysses]
         positions[0:N_ulysses] = positions[N_start:N_start + N_ulysses]
