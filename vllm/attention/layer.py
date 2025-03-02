17470565a25238b82a63de484833155f3e8b0748
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-01 18:00:19 -0800

    capture Ulysses operations w/ graph

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -226,7 +226,8 @@ class Attention(nn.Module):
 
             # Ulysses all-to-all 2/2
             c = output.view(self.SP, -1, self.num_heads, self.head_size)
-            torch.distributed.all_to_all_single(c, c_, group=self.device_group)
+            # torch.distributed.all_to_all_single(c, c_,
+            # group=self.device_group)
             output = torch.transpose(c, 0, 1).reshape(
                 -1, self.num_heads * self.SP * self.head_size)
 
