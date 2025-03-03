62a187dc15a9aa606157f9df3dd38fe6e57fc8fc
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-02 18:55:53 -0800

    capture Ulysses operations w/ graph

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -192,9 +192,9 @@ class Attention(nn.Module):
                     (self.num_heads + 2 * self.num_kv_heads) * self.head_size)
             qkv_ = torch.empty_like(qkv)
             # all-to-all
-            # torch.distributed.all_to_all_single(qkv_,
-            #                                     qkv,
-            #                                     group=self.device_group)
+            torch.distributed.all_to_all_single(qkv_,
+                                                qkv,
+                                                group=self.device_group)
             # unpack
             q_, k_, v_ = qkv_.split([
                 self.num_heads * self.head_size, self.num_kv_heads *
