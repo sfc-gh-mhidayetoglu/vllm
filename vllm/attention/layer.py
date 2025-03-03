b1c83522e0f6258ae7bd8eb1605142729c26c69f
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-03 14:17:01 -0800

    capture Ulysses operations w/ graph

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -10,7 +10,7 @@ import vllm.envs as envs
 from vllm.attention import AttentionMetadata, AttentionType
 from vllm.attention.selector import backend_name_to_enum, get_attn_backend
 from vllm.config import CacheConfig, get_current_vllm_config
-# from vllm.distributed.communication_op import sequence_all_to_all
+from vllm.distributed.communication_op import sequence_all_to_all
 from vllm.distributed.parallel_state import get_sp_group
 from vllm.forward_context import ForwardContext, get_forward_context
 from vllm.model_executor.layers.quantization.base_config import (
@@ -191,11 +191,11 @@ class Attention(nn.Module):
                     -1,
                     (self.num_heads + 2 * self.num_kv_heads) * self.head_size)
             # all-to-all
-            # qkv_ = sequence_all_to_all(qkv)
-            qkv_ = torch.empty_like(qkv)
-            torch.distributed.all_to_all_single(qkv_,
-                                                qkv,
-                                                group=self.device_group)
+            qkv_ = sequence_all_to_all(qkv)
+            # qkv_ = torch.empty_like(qkv)
+            # torch.distributed.all_to_all_single(qkv_,
+            #                                     qkv,
+            #                                     group=self.device_group)
             # unpack
             q_, k_, v_ = qkv_.split([
                 self.num_heads * self.head_size, self.num_kv_heads *
