947ffdc68853311fd332c0c74395dd5e4d6ce241
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-03 13:49:38 -0800

    capture Ulysses operations w/ graph

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -311,9 +311,9 @@ class GroupCoordinator:
 
     def all_to_all(self, input_: torch.Tensor) -> torch.Tensor:
         # torch.distributed.all_reduce(input_, group=self.device_group)
-        # output = input_
-        output = torch.empty_like(input_)
-        torch.distributed.all_to_all_single(output, input_, self.device_group)
+        output = input_
+        # output = torch.empty_like(input_)
+        # torch.distributed.all_to_all_single(output, input_, self.device_group)
         return output
 
     def all_reduce(self, input_: torch.Tensor) -> torch.Tensor:
