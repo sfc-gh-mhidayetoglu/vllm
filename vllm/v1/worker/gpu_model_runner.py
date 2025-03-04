c3cff9afc38c026745fdffe005e28b489f948dee
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-04 11:27:17 -0800

    capture graph size x SP

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -1033,12 +1033,11 @@ class GPUModelRunner:
         # can reuse the memory pool allocated for the large shapes.
         SP = self.parallel_config.sequence_parallel_size
         with graph_capture(device=self.device):
-            pass
-            # for num_tokens in reversed(self.cudagraph_batch_sizes):
-            #     for _ in range(self.vllm_config.compilation_config.
-            #                    cudagraph_num_of_warmups):
-            #         self._dummy_run(num_tokens * SP)
-            #     self._dummy_run(num_tokens * SP)
+            for num_tokens in reversed(self.cudagraph_batch_sizes):
+                for _ in range(self.vllm_config.compilation_config.
+                               cudagraph_num_of_warmups):
+                    self._dummy_run(num_tokens * SP)
+                self._dummy_run(num_tokens * SP)
 
         end_time = time.perf_counter()
         end_free_gpu_memory = torch.cuda.mem_get_info()[0]
