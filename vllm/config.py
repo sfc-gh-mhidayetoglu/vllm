4a45b28079ef82f92cbca98812fb0300bc771b4e
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-03 16:55:11 -0800

    capture Ulysses operations w/ graph

--- vllm/config.py
+++ vllm/config.py
@@ -2934,7 +2934,7 @@ class CompilationConfig(BaseModel):
                 # v1 must split the graph on attention ops
                 # for piecewise cudagraph
                 self.splitting_ops = [
-                    "vllm.unified_attention",
+                    # "vllm.unified_attention",
                     "vllm.unified_attention_with_output",
                     "vllm.custom_all_to_all",
                 ]
