cce1af24f101d54eddc50688eedf4c9e0b5c48c1
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-03 16:23:38 -0800

    capture Ulysses operations w/ graph

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -427,7 +427,7 @@ def custom_all_to_all_fake(
 direct_register_custom_op(
     op_name="custom_all_to_all",
     op_func=custom_all_to_all,
-    mutates_args=[],
+    mutates_args=["output"],
     fake_impl=custom_all_to_all_fake,
     dispatch_key=current_platform.dispatch_key,
 )
