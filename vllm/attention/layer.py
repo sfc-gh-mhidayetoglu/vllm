00e8f675704381e1d0cbcab9391c1c7568220571
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-05 11:40:25 -0800

    reshape q, k, v, c instead of view

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -169,13 +169,13 @@ class Attention(nn.Module):
             # Reshape the query, key, and value tensors.
             # NOTE(woosuk): We do this outside the custom op to minimize the
             # CPU overheads from the non-CUDA-graph regions.
-            query = query.view(-1, self.num_heads, self.head_size)
-            output = output.view(-1, self.num_heads, self.head_size)
-            if key is not None:
-                key = key.view(-1, self.num_kv_heads, self.head_size)
-            if value is not None:
-                value = value.shape(-1, self.num_kv_heads,
-                                    self.head_size)  # contiguous()
+            # query = query.view(-1, self.num_heads, self.head_size)
+            # output = output.view(-1, self.num_heads, self.head_size)
+            # if key is not None:
+            #     key = key.view(-1, self.num_kv_heads, self.head_size)
+            # if value is not None:
+            #     value = value.shape(-1, self.num_kv_heads,
+            #                         self.head_size)  # contiguous()
             if self.use_direct_call:
                 forward_context: ForwardContext = get_forward_context()
                 ctx_attn_metadata = forward_context.attn_metadata
--- vllm/v1/attention/backends/flash_attn.py
+++ vllm/v1/attention/backends/flash_attn.py
@@ -230,14 +230,10 @@ class FlashAttentionImpl(AttentionImpl):
         # ],
         #                         dim=-1)
         # prepare
-        # q_ = q_.reshape(-1, self.num_heads, self.head_size)
-        # k_ = k_.reshape(-1, self.num_kv_heads, self.head_size)
-        # v_ = v_.reshape(-1, self.num_kv_heads, self.head_size)
-        # c_ = output.reshape((-1, self.num_heads, self.head_size))
-        q_ = query
-        k_ = key
-        v_ = value
-        c_ = output
+        q_ = query.reshape(-1, self.num_heads, self.head_size)
+        k_ = key.reshape(-1, self.num_kv_heads, self.head_size)
+        v_ = value.reshape(-1, self.num_kv_heads, self.head_size)
+        c_ = output.reshape((-1, self.num_heads, self.head_size))
 
         # if torch.distributed.get_rank() == 0:
         #     print(f"\n \
