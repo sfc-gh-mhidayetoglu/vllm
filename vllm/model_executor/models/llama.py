5a58e5d89450ea0198f965d0129f457b25761a84
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-04 16:07:06 -0800

    print attention metadata

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -561,16 +561,18 @@ class LlamaForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
         #     print(f"input_ids: {input_ids.shape}")
         #     print(f"positions: {positions.shape}")
         #     print(f"N {N}, SP {SP}, N_ranks {N_ranks} sum {sum(N_ranks)}")
-        if torch.distributed.get_rank() == 0:
-            print(f"numforward {self.numforward} N {N}")
         from vllm.forward_context import get_forward_context
         metadata = get_forward_context().attn_metadata
-        if metadata is not None:
+        if metadata is None:
+            if torch.distributed.get_rank() == 0:
+                print(f"numforward {self.numforward} N {N}")
+        else:
             self.numforward += 1
             if torch.distributed.get_rank() == 0:
                 # print(f"attn_metadata {get_forward_context().attn_metadata}")
-                print(f"actual tokens: {metadata.num_actual_tokens} seq. "
-                      f"lens: {list(metadata.seq_lens)}")
+                print(f"numforward {self.numforward} N {N} "
+                      f"actual tokens: {metadata.num_actual_tokens} "
+                      f"seq. lens: {metadata.seq_lens.tolist()}")
 
         # narrow the input
         input_ids[:N_ulysses] = input_ids[N_offset:N_offset + N_ulysses]
