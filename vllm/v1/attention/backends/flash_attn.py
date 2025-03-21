e45318dd54b570b8e3ff0cc44163fcb02a4b0465
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-21 14:05:16 -0700

    set SP -1

--- vllm/v1/attention/backends/flash_attn.py
+++ vllm/v1/attention/backends/flash_attn.py
@@ -210,12 +210,13 @@ class FlashAttentionImpl(AttentionImpl):
         # output.copy_(query)
         # return output
         # traceback.print_stack()
+        N = query.shape[0] * self.SP
         # Ulysses all-to-all 1/2
         # pack
         qkv = torch.cat(
-            (query.view(-1, self.SP, self.num_heads * self.head_size),
-             key.view(-1, self.SP, self.num_kv_heads * self.head_size),
-             value.view(-1, self.SP, self.num_kv_heads * self.head_size)),
+            (query.view(N, -1, self.num_heads * self.head_size),
+             key.view(N, -1, self.num_kv_heads * self.head_size),
+             value.view(N, -1, self.num_kv_heads * self.head_size)),
             dim=-1).transpose(0, 1).reshape(
                 -1, (self.num_heads + 2 * self.num_kv_heads) * self.head_size)
         # all-to-all
