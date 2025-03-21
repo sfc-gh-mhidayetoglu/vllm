84d6c49dcb4f55dbd6212c635bd9b57828cd6287
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-21 13:13:52 -0700

    revert division w/ sp_tp_size

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -113,22 +113,21 @@ class LlamaAttention(nn.Module):
         self.hidden_size = hidden_size
         tp_size = get_tp_group().world_size
         sp_size = get_sp_group().world_size
-        sp_tp_size = tp_size * sp_size
         self.total_num_heads = num_heads
-        assert self.total_num_heads % sp_tp_size == 0
-        self.num_heads = num_heads // sp_tp_size
+        assert self.total_num_heads % tp_size == 0
+        self.num_heads = num_heads // tp_size
         self.total_num_kv_heads = num_kv_heads
-        if self.total_num_kv_heads >= sp_tp_size:
+        if self.total_num_kv_heads >= tp_size:
             # Number of KV heads is greater than TP size, so we partition
             # the KV heads across multiple tensor parallel GPUs.
-            assert self.total_num_kv_heads % sp_tp_size == 0
+            assert self.total_num_kv_heads % tp_size == 0
             self.isreplicated = False
         else:
             # Number of KV heads is less than TP size, so we replicate
             # the KV heads across multiple tensor parallel GPUs.
-            assert sp_tp_size % self.total_num_kv_heads == 0
+            assert tp_size % self.total_num_kv_heads == 0
             self.isreplicated = True
-        self.num_kv_heads = max(1, self.total_num_kv_heads // sp_tp_size)
+        self.num_kv_heads = max(1, self.total_num_kv_heads // tp_size)
         # MistralConfig has an optional head_dim introduced by Mistral-Nemo
         self.head_dim = getattr(config, "head_dim",
                                 self.hidden_size // self.total_num_heads)
@@ -184,10 +183,10 @@ class LlamaAttention(nn.Module):
             sliding_window = None
 
         self.attn = Attention(
-            self.num_heads,
+            self.num_heads // sp_size,
             self.head_dim,
             self.scaling,
-            num_kv_heads=self.num_kv_heads,
+            num_kv_heads=self.num_kv_heads // sp_size,
             cache_config=cache_config,
             quant_config=quant_config,
             per_layer_sliding_window=sliding_window,
