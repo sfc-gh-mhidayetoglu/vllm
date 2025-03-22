b86557f244491e0646e2d9b312c8bd57bbb41787
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-22 12:45:18 -0700

    print profile test

--- vllm/v1/worker/gpu_worker.py
+++ vllm/v1/worker/gpu_worker.py
@@ -158,14 +158,14 @@ class Worker:
         torch.cuda.empty_cache()
         torch.cuda.reset_peak_memory_stats()
 
-        print("test profile run")
-        from vllm.distributed.parallel_state import get_world_group
-        get_world_group().barrier()
-
         _, total_gpu_memory = torch.cuda.mem_get_info()
         # Execute a forward pass with dummy inputs to profile the memory usage
         # of the model.
         self.model_runner.profile_run()
+
+        print("test profile run")
+        from vllm.distributed.parallel_state import get_world_group
+        get_world_group().barrier()
 
         free_gpu_memory, _ = torch.cuda.mem_get_info()
         # NOTE(woosuk): Here we assume that the other processes using the same
