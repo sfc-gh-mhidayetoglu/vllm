17b6b849d76ff776d07cc660c158e477eb0ca368
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-21 17:34:39 -0700

    test

--- vllm/v1/attention/backends/flash_attn.py
+++ vllm/v1/attention/backends/flash_attn.py
@@ -223,7 +223,9 @@ class FlashAttentionImpl(AttentionImpl):
                 device=key.device,
                 dtype=key.dtype)
             v_ = torch.empty_like(k_)
-            torch.all_gather_into_tensor(k_, key, group=self.device_group)
+            torch.distributed.all_gather_into_tensor(k_,
+                                                     key,
+                                                     group=self.device_group)
         else:
             # q = query.view(-1,
             #                self.SP, self.num_heads *
