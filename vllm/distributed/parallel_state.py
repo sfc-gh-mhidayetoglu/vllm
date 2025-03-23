dddf5bb8445eeada6b7e9b622441b871118368ec
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-23 11:32:27 -0700

    remove print

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -1062,7 +1062,6 @@ def initialize_model_parallel(
             range(i * tensor_model_parallel_size,
                   (i + 1) * tensor_model_parallel_size))
         group_ranks.append(ranks)
-    logger.debug("TP group ranks: %s", group_ranks)
     # message queue broadcaster is only used in tensor model parallel group
     _TP = init_model_parallel_group(group_ranks,
                                     get_world_group().local_rank,
@@ -1080,7 +1079,6 @@ def initialize_model_parallel(
     for i in range(num_pipeline_model_parallel_groups):
         ranks = list(range(i, world_size, num_pipeline_model_parallel_groups))
         group_ranks.append(ranks)
-    logger.debug("PP group ranks: %s", group_ranks)
     # pipeline parallel does not need custom allreduce
     _PP = init_model_parallel_group(group_ranks,
                                     get_world_group().local_rank,
@@ -1102,7 +1100,6 @@ def initialize_model_parallel(
                       (i + 1) * ulysses_model_parallel_size + j,
                       tensor_model_parallel_size))
             group_ranks.append(ranks)
-    logger.debug("SP group ranks: %s", group_ranks)
     _SP = init_model_parallel_group(group_ranks,
                                     get_world_group().local_rank,
                                     backend,
@@ -1117,7 +1114,6 @@ def initialize_model_parallel(
             range(i * ulysses_model_parallel_size,
                   (i + 1) * ulysses_model_parallel_size))
         group_ranks.append(ranks)
-    logger.debug("SP_TP group ranks: %s", group_ranks)
     _SP_TP = init_model_parallel_group(group_ranks,
                                        get_world_group().local_rank,
                                        backend,
