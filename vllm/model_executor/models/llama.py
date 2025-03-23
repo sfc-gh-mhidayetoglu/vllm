ad5d3a2ff7f2dd67e159e8a7bc3d50354d7e2e0a
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-22 18:21:55 -0700

    print attention

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -224,8 +224,7 @@ class LlamaAttention(nn.Module):
         # torch.distributed.barrier()
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         q, k = self.rotary_emb(positions, q, k)
-        # attn_output = self.attn(q, k, v, kv_cache, attn_metadata)
-        attn_output = q.contiguous()
+        attn_output = self.attn(q, k, v, kv_cache, attn_metadata)
         output, _ = self.o_proj(attn_output)
         return output
 
