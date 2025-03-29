1cdf8e9dfef303d24f078b0c40e5e21179a4bf82
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-03-28 18:35:24 -0700

    remove Ulysses from model attention

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -31,8 +31,8 @@ from transformers import LlamaConfig
 from vllm.attention import Attention
 from vllm.compilation.decorators import support_torch_compile
 from vllm.config import CacheConfig, VllmConfig
-from vllm.distributed import get_pp_group, get_tensor_model_parallel_world_size
-from vllm.distributed.parallel import get_sp_group
+from vllm.distributed import (get_pp_group, get_sp_group,
+                              get_tensor_model_parallel_world_size)
 from vllm.model_executor.layers.activation import SiluAndMul
 from vllm.model_executor.layers.layernorm import RMSNorm
 from vllm.model_executor.layers.linear import (MergedColumnParallelLinear,
