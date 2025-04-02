3fb1d00f2e12e43465f9a00a70094c84e965af41
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-02 13:09:46 -0700

    remove forward propagation in Qwen

--- vllm/model_executor/models/qwen2.py
+++ vllm/model_executor/models/qwen2.py
@@ -334,12 +334,13 @@ class Qwen2Model(nn.Module):
             assert intermediate_tensors is not None
             hidden_states = intermediate_tensors["hidden_states"]
             residual = intermediate_tensors["residual"]
-        for layer in self.layers[self.start_layer:self.end_layer]:
-            hidden_states, residual = layer(
-                positions,
-                hidden_states,
-                residual,
-            )
+        residual = hidden_state
+        # for layer in self.layers[self.start_layer:self.end_layer]:
+        #     hidden_states, residual = layer(
+        #         positions,
+        #         hidden_states,
+        #         residual,
+        #     )
         if not get_pp_group().is_last_rank:
             return IntermediateTensors({
                 "hidden_states": hidden_states,
