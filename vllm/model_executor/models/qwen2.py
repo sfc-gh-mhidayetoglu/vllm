728fbd88e01067c11ea58b4c2293bda95378b4b3
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-03 06:20:47 +0000

    fix typo

--- vllm/model_executor/models/qwen2.py
+++ vllm/model_executor/models/qwen2.py
@@ -334,7 +334,7 @@ class Qwen2Model(nn.Module):
             assert intermediate_tensors is not None
             hidden_states = intermediate_tensors["hidden_states"]
             residual = intermediate_tensors["residual"]
-        residual = hidden_state
+        residual = hidden_states
         # for layer in self.layers[self.start_layer:self.end_layer]:
         #     hidden_states, residual = layer(
         #         positions,
