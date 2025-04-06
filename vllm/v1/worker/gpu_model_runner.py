abe5cd14ad35f8453039ab83a8dfb7c5fad05a78
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-06 13:20:05 -0700

    test model runner

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -1179,6 +1179,7 @@ class GPUModelRunner(LoRAModelRunnerMixin):
         logger.info("Model loading took %.4f GB and %.6f seconds",
                     self.model_memory_usage / float(2**30),
                     time_after_load - time_before_load)
+        print("test")
 
     def _get_prompt_logprobs_dict(
         self,
