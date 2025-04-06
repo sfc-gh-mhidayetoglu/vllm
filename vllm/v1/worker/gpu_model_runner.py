ebb95146914b1c1bf98fb10017df256c0a6da601
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-06 13:27:56 -0700

    test model runner

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -1180,14 +1180,18 @@ class GPUModelRunner(LoRAModelRunnerMixin):
                     self.model_memory_usage / float(2**30),
                     time_after_load - time_before_load)
         print("test")
-        def custom_forward(*args, **kwargs):
-            print("Custom forward called!")
-            # You can add pre-processing code here
-            result = self.model.forward(*args, **kwargs)
-            # You can add post-processing code here
-            return result
+        def monkeypatch_forward():
+            original_forward = self.model.forward
+            def custom_forward(*args, **kwargs):
+                print("Custom forward called!")
+                # You can add pre-processing code here
+                result = original_forward(*args, **kwargs)
+                # You can add post-processing code here
+                return result
 
-        self.model.forward = custom_forward
+            self.model.forward = custom_forward
+
+        self.monkeypatch_forward()
 
     def _get_prompt_logprobs_dict(
         self,
