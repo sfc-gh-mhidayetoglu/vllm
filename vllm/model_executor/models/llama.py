cd912124116f577c44fb7e5a5cd9e26ba7513427
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-07 23:03:47 -0700

    remove comment

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -201,9 +201,8 @@ class LlamaAttention(nn.Module):
         qkv, _ = self.qkv_proj(hidden_states)
         q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
         q, k = self.rotary_emb(positions, q, k)
-        # attn_output = self.attn(q, k, v)
-        # output, _ = self.o_proj(attn_output)
-        output, _ = self.o_proj(q.contiguous())
+        attn_output = self.attn(q, k, v)
+        output, _ = self.o_proj(attn_output)
         return output
 
 
