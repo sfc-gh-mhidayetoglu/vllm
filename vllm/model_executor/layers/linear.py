2ce776f7000854f4699ca7cea2a95cf6e04572f6
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-10 10:48:18 -0700

    remove linear changes

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -186,32 +186,9 @@ class UnquantizedLinearMethod(LinearMethodBase):
     def apply(self,
               layer: torch.nn.Module,
               x: torch.Tensor,
-              bias: Optional[torch.Tensor] = None,
-              sp_tp_mode: bool = False,
-              column_parallel: bool = False,
-              output_partition_sizes: list = None) -> torch.Tensor:
-        if sp_tp_mode:
-            from vllm.distributed.parallel_state import get_sp_group
-            sp_size = get_sp_group().world_size
-            sp_rank = get_sp_group().rank_in_group
-            # sp_size = get_sequence_model_parallel_world_size()
-            # sp_rank = get_sequence_model_parallel_rank()
-            if not column_parallel:
-                assert layer.weight.shape[1] % sp_size == 0
-                chunk_size = layer.weight.shape[1] // sp_size
-                weight = layer.weight.split(chunk_size, dim=1)[sp_rank]
-            else:
-                assert layer.weight.shape[0] % sp_size == 0
-                chunk_sizes = []
-                for size in output_partition_sizes:
-                    chunk_size = size // sp_size
-                    chunk_sizes.extend([chunk_size] * sp_size)
-                split = layer.weight.split(chunk_sizes, dim=0)
-                weight = torch.cat([split[i] for i in range(sp_rank, len(split), sp_size)])
-            # print("TPSP", (sp_size, chunk_size, layer.weight.shape, weight.shape, x.shape))
-        else:
-            weight = layer.weight
-        return F.linear(x, weight, bias)
+              bias: Optional[torch.Tensor] = None) -> torch.Tensor:
+
+        return F.linear(x, layer.weight, bias)
 
 
 class LinearBase(torch.nn.Module):
@@ -494,12 +471,12 @@ class ColumnParallelLinear(LinearBase):
 
         # Matrix multiply.
         assert self.quant_method is not None
-#         output_parallel = self.quant_method.apply(self, input_, bias)
-        from vllm.v1.worker.gpu_model_runner import SP_TP_MODE
-        output_parallel = self.quant_method.apply(self, input_, bias,
-                                                  sp_tp_mode=SP_TP_MODE,
-                                                  column_parallel=True,
-                                                  output_partition_sizes=self.output_partition_sizes)
+        output_parallel = self.quant_method.apply(self, input_, bias)
+        # from vllm.v1.worker.gpu_model_runner import SP_TP_MODE
+        # output_parallel = self.quant_method.apply(self, input_, bias,
+        #                                           sp_tp_mode=SP_TP_MODE,
+        #                                           column_parallel=True,
+        #                                           output_partition_sizes=self.output_partition_sizes)
         if self.gather_output:
             assert True, "gather_output is not supported"
             # All-gather across the partitions.
@@ -1271,18 +1248,18 @@ class RowParallelLinear(LinearBase):
     def forward(
         self, input_
     ) -> Union[torch.Tensor, tuple[torch.Tensor, Optional[Parameter]]]:
-        from vllm.distributed.parallel_state import get_sp_tp_group
-        sp_tp_size = get_sp_tp_group().world_size
-        sp_tp_rank = get_sp_tp_group().rank_in_group
+        # from vllm.distributed.parallel_state import get_sp_tp_group
+        # sp_tp_size = get_sp_tp_group().world_size
+        # sp_tp_rank = get_sp_tp_group().rank_in_group
 
         if self.input_is_parallel:
             #print("PARALLEL", input_.shape)
             input_parallel = input_
-        elif sp_tp_mode:
-            #print("SPTPMODE", input_.shape)
-            splitted_input = split_tensor_along_last_dim(
-                input_, num_partitions=sp_tp_size)
-            input_parallel = splitted_input[sp_tp_rank].contiguous()
+        # elif sp_tp_mode:
+        #     #print("SPTPMODE", input_.shape)
+        #     splitted_input = split_tensor_along_last_dim(
+        #         input_, num_partitions=sp_tp_size)
+        #     input_parallel = splitted_input[sp_tp_rank].contiguous()
         else:
             #print("NOSPTPMODE", input_.shape)
             tp_rank = get_tensor_model_parallel_rank()
@@ -1294,17 +1271,18 @@ class RowParallelLinear(LinearBase):
         assert self.quant_method is not None
         # Only fuse bias add into GEMM for rank 0 (this ensures that
         # bias will not get added more than once in TP>1 case)
-        from vllm.v1.worker.gpu_model_runner import SP_TP_MODE
-        if SP_TP_MODE:
-            bias_ = None if (sp_tp_rank > 0 or self.skip_bias_add) else self.bias
-        else:
-            bias_ = None if (self.tp_rank > 0 or self.skip_bias_add) else self.bias
+        bias_ = None if (self.tp_rank > 0 or self.skip_bias_add) else self.bias
+        # from vllm.v1.worker.gpu_model_runner import SP_TP_MODE
+        # if SP_TP_MODE:
+        #     bias_ = None if (sp_tp_rank > 0 or self.skip_bias_add) else self.bias
+        # else:
+        #     bias_ = None if (self.tp_rank > 0 or self.skip_bias_add) else self.bias
         output_parallel = self.quant_method.apply(self,
                                                   input_parallel,
                                                   bias=bias_)
         if self.reduce_results and self.tp_size > 1:
-            # output = tensor_model_parallel_all_reduce(output_parallel)
-            output = get_sp_tp_group().allreduce(output_parallel)
+            output = tensor_model_parallel_all_reduce(output_parallel)
+            # output = get_sp_tp_group().allreduce(output_parallel)
         else:
             output = output_parallel
 
--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -596,11 +596,13 @@ class LlamaForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
         #               f"seq. lens: {metadata.seq_lens.tolist()}")
 
         from vllm.v1.worker.gpu_model_runner import SP_TP_MODE
+        from vllm.v1.worker.gpu_worker import SS_PROFILE_RUN
         metadata = get_forward_context().attn_metadata
         if torch.distributed.get_rank() == 0:
             print(f"numiter: {self.numiter} "
                   f"input_ids: {input_ids.shape} "
-                  f"SP_TP_MODE: {SP_TP_MODE}")
+                  f"SP_TP_MODE: {SP_TP_MODE} "
+                  f"SS_PROFILE_RUN: {SS_PROFILE_RUN}")
             if metadata is None:
                 print("metadata: None")
             else:
@@ -609,10 +611,10 @@ class LlamaForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
                       f"seq. lens: {metadata.seq_lens.tolist()}")
         self.numiter += 1
 
-        if SP_TP_MODE is None or SP_TP_MODE is False:
+        if SS_PROFILE_RUN or SP_TP_MODE is False:
             model_output = self.model(input_ids, positions,
                                       intermediate_tensors, inputs_embeds)
-        if SP_TP_MODE is None or SP_TP_MODE is True:
+        if SS_PROFILE_RUN is None or SP_TP_MODE is True:
             model_output = self.model_tp(input_ids, positions,
                                          intermediate_tensors, inputs_embeds)
         return model_output
