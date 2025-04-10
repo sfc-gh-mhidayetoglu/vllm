42466bce21afac482d48bc82404e291aaee3d0b1
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-10 16:43:01 -0700

    test

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -371,8 +371,8 @@ class LlamaModel(nn.Module):
             hidden_states = intermediate_tensors["hidden_states"]
             residual = intermediate_tensors["residual"]
 
-        if torch.distributed.get_rank() == 0:
-            print(f"hello from forward input_ids {input_ids.shape}")
+        # if torch.distributed.get_rank() == 0:
+        #     print(f"hello from forward input_ids {input_ids.shape}")
         for layer in self.layers[self.start_layer:self.end_layer]:
             # for layer in self.layers[0:1]:
             hidden_states, residual = layer(positions, hidden_states, residual)
