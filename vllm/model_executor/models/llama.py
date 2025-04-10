f4133c71d457fee140f91703e20fbc6a3b05ed1e
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-10 16:08:08 -0700

    test

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -219,8 +219,8 @@ class LlamaAttention(nn.Module):
         q, k = self.rotary_emb(positions, q, k)
         attn_output = self.attn(q, k, v)
 
-        output, _ = self.o_proj(attn_output)
-        return output
+        # output, _ = self.o_proj(attn_output)
+        return attn_output
 
 
 class LlamaDecoderLayer(nn.Module):
