6d79ccac6e093abdffbf202820bd294b030307ba
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-09 21:04:01 -0700

    disable padding in SP_TP_MODE

--- vllm/v1/worker/gpu_model_runner.py
+++ vllm/v1/worker/gpu_model_runner.py
@@ -987,10 +987,10 @@ class GPUModelRunner(LoRAModelRunnerMixin):
                 num_input_tokens = self.vllm_config.pad_for_cudagraph(
                     num_input_tokens)
             else:
-                if torch.distributed.get_rank() == 0:
-                    print(f"num_scheduled_tokens: {num_scheduled_tokens} >= {SP_TP_THRESHOLD}")
                 pass
         else:
+            if torch.distributed.get_rank() == 0:
+                print(f"num_scheduled_tokens: {num_scheduled_tokens} >= {SP_TP_THRESHOLD}")
             # add padding to the batch size to make it a multiple of SP
             SP = self.parallel_config.sequence_parallel_size
             num_input_tokens = (num_scheduled_tokens + SP - 1) // SP * SP
