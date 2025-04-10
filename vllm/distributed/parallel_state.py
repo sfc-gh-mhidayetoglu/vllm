ba588ea1289afdb31c8e1c528ddf9b27926546d7
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-10 16:53:39 -0700

    test

--- vllm/distributed/parallel_state.py
+++ vllm/distributed/parallel_state.py
@@ -810,7 +810,7 @@ def graph_capture(device: torch.device):
     """
     context = GraphCaptureContext(torch.cuda.Stream(device=device))
     with get_tp_group().graph_capture(context), get_pp_group().graph_capture(
-            context):
+            context), get_sp_tp_group().graph_capture(context):
         yield context
 
 
