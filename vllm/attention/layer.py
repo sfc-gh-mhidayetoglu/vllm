bdf72ba230f38e157ce55d92c7fe19da06ab4209
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-11 20:36:01 -0700

    replace alltoall with allreduce

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -206,11 +206,13 @@ class Attention(nn.Module):
                 #     value = value.view(-1, self.num_kv_heads, self.head_size)
                 from vllm.v1.worker.gpu_model_runner import SP_TP_MODE
                 if SP_TP_MODE:
-                    recv = torch.empty_like(query)
+                    # recv = torch.empty_like(query)
                     # torch.distributed.all_to_all_single(
                     #     recv,
                     #     query.contiguous(),
                     #     group=get_sp_group().device_group)
+                    torch.distributed.all_reduce(
+                        query, group=get_sp_group().device_group)
                     recv = query
                 else:
                     recv = query
