8947c4fffdfe414dcca049ac1a048a89b166c0f4
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-12 11:31:01 -0700

    print

--- vllm/distributed/device_communicators/cuda_communicator.py
+++ vllm/distributed/device_communicators/cuda_communicator.py
@@ -56,9 +56,8 @@ class CudaCommunicator(DeviceCommunicatorBase):
             )
 
         self.alltoall_comm: Optional[CustomAlltoall] = None
-        use_custom_alltoall = True
         if use_custom_allreduce and self.world_size > 1:
-            self.alltoall_comm = CustomAlltoall(
+            self.alltoall_comm = CustomAllreduce(
                 group=self.cpu_group,
                 device=self.device,
             )
