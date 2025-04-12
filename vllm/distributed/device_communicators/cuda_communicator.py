2c373dab691476feab20883eba1b2cdcdb0f90fc
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-12 10:03:20 -0700

    create CustomAlltoall

--- vllm/distributed/device_communicators/cuda_communicator.py
+++ vllm/distributed/device_communicators/cuda_communicator.py
@@ -50,6 +50,12 @@ class CudaCommunicator(DeviceCommunicatorBase):
             )
 
         self.alltoall_comm: Optional[CustomAlltoall] = None
+        use_custom_alltoall = True
+        if use_custom_alltoall and self.world_size > 1:
+            self.alltoall_comm = CustomAlltoall(
+                group=self.cpu_group,
+                device=self.device,
+            )
 
     def all_reduce(self, input_):
         # always try custom allreduce first,
