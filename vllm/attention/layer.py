af982c3a2449b930c9fb3e28c7c03127f1c29c53
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-13 20:30:11 -0700

    print

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -232,7 +232,10 @@ class Attention(nn.Module):
             else:
                 torch.ops.vllm.unified_attention_with_output(
                     query, key, value, output, self.layer_name)
-            return output.view(-1, hidden_size)
+                
+            out = get_sp_group().all_to_all(output)
+
+            return out.view(-1, hidden_size)
         else:
             if self.use_direct_call:
                 forward_context = get_forward_context()
