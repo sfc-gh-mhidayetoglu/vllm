628d084fc70c17dee5bf271e93dd9a9564367279
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-13 22:31:39 -0700

    print

--- vllm/distributed/device_communicators/cuda_communicator.py
+++ vllm/distributed/device_communicators/cuda_communicator.py
@@ -79,7 +79,8 @@ class CudaCommunicator(DeviceCommunicatorBase):
     
     def all_to_all(self, input_):
         out = torch.empty_like(input_)
-        torch.distributed.all_to_all_single(out, input_, group=self.device_group)
+        # torch.distributed.all_to_all_single(out, input_, group=self.device_group)
+        out.copy_(input_)
         return out
 
     def all_reduce(self, input_):
