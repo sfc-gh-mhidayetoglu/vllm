5c395a6e647302fd8ff43a67962a9250133088c3
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-13 22:12:46 -0700

    print

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -223,11 +223,11 @@ class Attention(nn.Module):
                     # qkv_ = get_sp_group().all_to_all(qkv)
                     # qkv_ = qkv
                     # unpack
-                    # q_, k_, v_ = qkv_.split([
-                    #     self.num_heads * self.head_size, self.num_kv_heads *
-                    #     self.head_size, self.num_kv_heads * self.head_size
-                    # ],
-                    #                         dim=-1)
+                    q_, k_, v_ = qkv_.split([
+                        self.num_heads * self.head_size, self.num_kv_heads *
+                        self.head_size, self.num_kv_heads * self.head_size
+                    ],
+                                            dim=-1)
                     # prepare
                     q_ = query.reshape(-1, self.num_heads, self.head_size)
                     k_ = key.reshape(-1, self.num_kv_heads, self.head_size)
