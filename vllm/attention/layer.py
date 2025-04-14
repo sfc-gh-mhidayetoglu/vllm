71d78d2cc2377038f76a41c832cf1468c7bb9936
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-13 21:52:40 -0700

    print

--- vllm/attention/layer.py
+++ vllm/attention/layer.py
@@ -229,9 +229,9 @@ class Attention(nn.Module):
                     # ],
                     #                         dim=-1)
                     # prepare
-                    q_ = q_.reshape(-1, self.num_heads, self.head_size)
-                    k_ = k_.reshape(-1, self.num_kv_heads, self.head_size)
-                    v_ = v_.reshape(-1, self.num_kv_heads, self.head_size)
+                    q_ = query.reshape(-1, self.num_heads, self.head_size)
+                    k_ = key.reshape(-1, self.num_kv_heads, self.head_size)
+                    v_ = value.reshape(-1, self.num_kv_heads, self.head_size)
                     c_ = output.view(-1, self.num_heads, self.head_size)
 
             if self.use_direct_call:
