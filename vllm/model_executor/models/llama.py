a9bb0c958bc28df4ed99daa565d269d5b04298b0
Mert Hidayetoglu <mert.hidayetoglu@snowflake.com>
2025-04-18 13:59:06 -0700

    print

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -557,7 +557,10 @@ class LlamaForCausalLM(nn.Module, SupportsLoRA, SupportsPP):
         self.make_empty_intermediate_tensors = (
             self.model.make_empty_intermediate_tensors)
 
-        # self.numiter = 0
+        self.numiter = 0
+        self.prefill = 0
+        self.decode = 0
+        self.mixed = 0
 
     def _init_model(self, vllm_config: VllmConfig, prefix: str = ""):
         return LlamaModel(vllm_config=vllm_config, prefix=prefix)
